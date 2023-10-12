9049c31a82aaa4cb7ec2ac8749015cac0e8f0bed
oussema.sellami <oussema.sellami@esprit.tn>
2023-10-12 11:57:55 +0100

    crud author + jointure

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
@@ -20,6 +25,74 @@ class AuthorController extends AbstractController
         return $this->render('author/index.html.twig', [
             'controller_name' => 'AuthorController',
         ]);
+    }
+
+    #[Route('/showdbauthor', name: 'showdbauthor')]
+    public function showdbauthor(AuthorRepository $authorRepository): Response
+    {
+        $author = $authorRepository->findAll();
+        return $this->render('author/showdbauthor.html.twig', [
+            'author' => $author
+        ]);
+    }
+
+    #[Route('/addauthor', name: 'addauthor')]
+    public function addauthor(ManagerRegistry $managerRegistry): Response
+    {
+        $x = $managerRegistry->getManager();
+        $author = new Author();
+        $author->setUsername("3a54new");
+        $author->setEmail("3a54new@esprit.tn");
+        $x->persist($author);
+        $x->flush();
+        return new Response(" great add");
+    }
+
+    #[Route('/addformauthor', name: 'addformauthor')]
+    public function addformauthor(ManagerRegistry $managerRegistry, Request $req): Response
+    {
+        $x = $managerRegistry->getManager();
+        $author = new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($req);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $x->persist($author);
+            $x->flush();
+
+            return $this->redirectToRoute('showdbauthor');
+        }
+        return $this->renderForm('author/addformauthor.html.twig', [
+            'f' => $form
+        ]);
+    }
+
+    #[Route('/editauthor/{id}', name: 'editauthor')]
+    public function editauthor($id, AuthorRepository $authorRepository, ManagerRegistry $managerRegistry, Request $req): Response
+    {
+        //var_dump($id) . die();
+        $x = $managerRegistry->getManager();
+        $dataid = $authorRepository->find($id);
+        // var_dump($dataid) . die();
+        $form = $this->createForm(AuthorType::class, $dataid);
+        $form->handleRequest($req);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $x->persist($dataid);
+            $x->flush();
+            return $this->redirectToRoute('showdbauthor');
+        }
+        return $this->renderForm('author/editauthor.html.twig', [
+            'x' => $form
+        ]);
+    }
+
+    #[Route('/deleteauthor/{id}', name: 'deleteauthor')]
+    public function deleteauthor($id, ManagerRegistry $managerRegistry, AuthorRepository $authorRepository): Response
+    {
+        $em = $managerRegistry->getManager();
+        $dataid = $authorRepository->find($id);
+        $em->remove($dataid);
+        $em->flush();
+        return $this->redirectToRoute('showdbauthor');
     }
 
     #[Route('/showauthor/{name}', name: 'app_showauthor')]
---
+++ src/Entity/Book.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\Column]
+    private ?int $ref = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $publicationdate = null;
+
+    #[ORM\Column]
+    private ?bool $published = null;
+
+    #[ORM\ManyToOne(inversedBy: 'books')]
+    private ?Author $author = null;
+
+    public function getRef(): ?int
+    {
+        return $this->ref;
+    }
+
+    public function setRef(string $ref): static
+    {
+        $this->ref = $ref;
+
+        return $this;
+    }
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPublicationdate(): ?\DateTimeInterface
+    {
+        return $this->publicationdate;
+    }
+
+    public function setPublicationdate(\DateTimeInterface $publicationdate): static
+    {
+        $this->publicationdate = $publicationdate;
+
+        return $this;
+    }
+
+    public function isPublished(): ?bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): static
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Author $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
